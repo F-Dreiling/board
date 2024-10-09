58e87e02752b0a63ef791aaf8016f95efc3cf235
FX Drooklyn <dr.f380@hotmail.com>
2024-10-09 16:44:27 +0200

    setup

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,16 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): JsonResponse
+    #[Route('/', name: 'home')]
+    public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
-        ]);
+        return new Response(content: '<h1>Welcome</h1>');
+    }
+
+    #[Route('/custom/{name?}', name: 'custom')]
+    public function custom(Request $req)
+    {
+        dump($req);
+        return new Response(content: '<h1>Welcome mate</h1>');
     }
 }
