1caf453eb3489c456867e19d37c503329bb6c7a4
FX Drooklyn <dr.f380@hotmail.com>
2024-10-10 12:26:50 +0200

    twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,13 +13,19 @@ class MainController extends AbstractController
     #[Route('/', name: 'home')]
     public function index()
     {
-        return new Response(content: '<h1>Welcome</h1>');
+        //return new Response(content: '<h1>Welcome</h1>');
+        return $this->render(view:'home/index.html.twig');
     }
 
     #[Route('/custom/{name?}', name: 'custom')]
     public function custom(Request $req)
     {
-        dump($req);
-        return new Response(content: '<h1>Welcome mate</h1>');
+        //dump($req);
+        $name = $req->get(key:'name');
+        
+        //return new Response(content: '<h1>Welcome '. $name . '!</h1>');
+        return $this->render(view:'home/custom.html.twig', parameters: [
+            'name' => $name
+        ]);
     }
 }
