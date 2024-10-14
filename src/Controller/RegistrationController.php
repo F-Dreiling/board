a95c41afc94acab9cad36db9de027708194cd29c
FX Drooklyn <dr.f380@hotmail.com>
2024-10-14 18:28:17 +0200

    Login

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'register')]
+    public function register(Request $req, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        // DON'T do this for REAL
+        $form = $this->createFormBuilder()
+            ->add(child: 'username')
+            ->add(child: 'password', type: RepeatedType::class, options: [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password']
+            ])
+            ->add(child: 'register', type: SubmitType::class, options: [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
+            ->getForm()
+        ;
+
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+
+            $user = new User();
+            $user->setUsername($data['username']);
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $data['password']
+            );
+            $user->setPassword($hashedPassword);
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('login'));
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
